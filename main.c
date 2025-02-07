171bde227570e86f33a34f67b48c2bbf1eb32676
Stanhen <stanislashenocq@gmail.com>
2025-02-07 09:46:31 +0100

    afficher Tab

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
-int main(){}
+#include <stdio.h>
+#include <stdlib.h>
+int* allouerTableauEntiersV1(int const taille) {
+    int *t = calloc(taille,sizeof(int));
+    return t;
+}
+void afficherTab(int tab[], int const taille) {
+        if(tab!=NULL) {
+            for(int i=0; i<taille; i++) {
+                printf("%d", tab[i]);
+            }
+            printf("\n");
+    }
+}
+
+void allouerTableauEntiersV2(int** pTab, int const taille) {
+    int* t = calloc(taille,sizeof(int));
+    int**=t;
+}
+
+int main(void) {
+    int * tab=NULL;
+    int taille=3;
+    allouerTableauEntiersV2(pTab, taille);
+    return 0;
+}
