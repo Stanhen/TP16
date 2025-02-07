2346c720c6e829b263f9fcd5e5bf517eb723326b
Stanhen <stanislashenocq@gmail.com>
2025-02-07 10:02:07 +0100

    version 2

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int* allouerTableauEntiersV1(int const taille) {
     int *t = calloc(taille,sizeof(int));
     return t;
 }
+
 void afficherTab(int tab[], int const taille) {
         if(tab!=NULL) {
             for(int i=0; i<taille; i++) {
@@ -14,13 +16,15 @@ void afficherTab(int tab[], int const taille) {
 }
 
 void allouerTableauEntiersV2(int** pTab, int const taille) {
-    int* t = calloc(taille,sizeof(int));
-    int**=t;
+    *pTab = (int*)calloc(taille,sizeof(int));
 }
 
 int main(void) {
     int * tab=NULL;
-    int taille=3;
-    allouerTableauEntiersV2(pTab, taille);
+    int taille=10;
+    allouerTableauEntiersV2(&tab, taille);
+    afficherTab(tab,taille);
+    free(tab);
+    tab=NULL;
     return 0;
 }
